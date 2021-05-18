4c3d785b7062006b36344b72478b574d51a793d4
Kesinee Powers <56649145+KesineeP@users.noreply.github.com>
2021-05-18 19:25:13 -0400

    Create Actions and Reducers

---
+++ src/index.js
@@ -0,0 +1,63 @@
+import { createStore, combineReducers} from 'redux'
+console.clear();
+
+//Action Creators(form)
+const createPolicy = (name, amount) => {
+    return {//Action (a form in our analogy)
+        types: 'CREATE_POLICY',
+        payload: {
+            name: name,
+            amount: amount
+        }
+    };
+};
+
+const deletePolicy = (name) => {
+    return {
+        type: 'DELETE_POLICY',
+        payload: {
+            name: name
+        }
+    };
+};
+
+const createClaim = (name, amountOfMoneyToCollect) => {
+    return {
+        type: 'CREATE_CLAIM',
+        payload: {
+            name: name,
+            amountOfMoneyToCollect: amountOfMoneyToCollect
+        }
+    };
+};
+
+//Reducers(Department)
+const claimhistory = (oldListOfClaims = [], action) => {
+    if(action.type === 'CREATE_CLAIM') {
+        //we care about this action(form)
+        return [...oldListOfClaims, action.payload]
+    }
+    //else we don't care this action(form)
+    return oldListOfClaims;
+}
+
+const accounting = (bagOfMoney = 100, action) => {
+    if(action.type === 'CREATE_CLAIM') {
+        return bagOfMoney - action.payload.amountOfMoneyToCollect;
+    } else if(action.type === 'CREATE_POLICY') {
+        return bagOfMoney + action.payload.amount;
+    } else {
+        return bagOfMoney;
+    };
+};
+
+const policies = (listOfPolicies = [], action) => {
+    if(action.type === 'CREATE_POLICY') {
+        return [...listOfPolicies, action.payload.name]
+    } else if(action.type === "DELETE_POLICy") {
+        return listOfPolicies.filter(name => name !== action.payload.name);
+    }
+    return listOfPolicies;
+};
+
+
